56fad04a3894a0ade3fece586a2831090f64c712
Cáceres Rodolfo <caceresrodolfo28@gmail.com>
2021-04-02 16:54:41 -0300

    feat: Middleware is added in routes

--- src/routes/balances.routes.js
+++ src/routes/balances.routes.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const { getBalance } = require('../controllers/balances.ctrl');
+const TokenValidation = require('../middlewares/verify.auth');
 
 /* Se define un endpoint para obtener el balance y 
 se le asigna el contralador que manejara la peticion */
-router.get('/:id', getBalance);
+router.get('/:id', TokenValidation, getBalance);
 
 module.exports = router;
--- src/routes/operations.routes.js
+++ src/routes/operations.routes.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const TokenValidation = require('../middlewares/verify.auth');
 const {
   showLastOperations,
   addOperation,
@@ -12,11 +13,11 @@ const {
 /* Se definen los endpoints para las operacines y 
 a cada uno se le asigna un contralador que 
 manejara la peticion */
-router.get("/", showLastOperations);
-router.post("/", addOperation);
-router.delete("/:id", deleteOperation);
-router.put("/:id", updateOperation);
-router.get("/entries", showEntriesOperations);
-router.get("/outputs", showOutputsOperations);
+router.get("/", TokenValidation, showLastOperations);
+router.post("/", TokenValidation, addOperation);
+router.delete("/:id", TokenValidation, deleteOperation);
+router.put("/:id", TokenValidation, updateOperation);
+router.get("/entries", TokenValidation, showEntriesOperations);
+router.get("/outputs", TokenValidation, showOutputsOperations);
 
 module.exports = router;

bcf31c47465097244d42a6d954e53721a38d3ffc
Cáceres Rodolfo <caceresrodolfo28@gmail.com>
2021-04-15 10:19:47 -0300

    CORS configuration is added

--- src/config/server.js
+++ src/config/server.js
@@ -10,13 +10,26 @@ module.exports = app => {
   /* --------------- SETTINGS --------------- */
   /* Se configura el puerto donde escuchara el servidor */
   app.set("port", process.env.PORT || 4000);
-  
+
   /* --------------- MIDDLEWARES --------------- */
   /* Se configura los middlewares */
   app.use(express.json());
-  app.use(cors());
-  
-  if(process.env.NODE_ENV === 'development') {
+  // Configurar cabeceras y cors
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+    );
+    res.header(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, DELETE"
+    );
+    res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+    next();
+  });
+
+  if (process.env.NODE_ENV === "development") {
     const morgan = require("morgan");
     app.use(morgan("dev"));
   }
